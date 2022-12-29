95ad6cbf27b592fd57e9234dc9355ff98c63bb04
SIYAKS-ARES <114436845+SIYAKS-ARES@users.noreply.github.com>
2022-12-29 14:48:27 +0300

    First Commit

---
+++ SifreDogrulama.java
@@ -0,0 +1,20 @@
+package Orneklerim;
+
+import java.util.Scanner;
+
+public class SifreDogrulama {
+    public static void main(String[] args){
+        String pass = "12345";
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Şifrenizi giriniz: ");
+
+        String userpass = scanner.nextLine();
+
+        if (userpass.equals(pass)){
+            System.out.println("Giriş Başarılı! ");
+        }
+        else{
+            System.out.println("Giriş Başarısız!");
+        }
+    }
+}
